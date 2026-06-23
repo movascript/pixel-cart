6efda54c4014dc82b6705336d5a668d45dcdb095
Mohammad Valadi <movascript@gmail.com>
2026-06-23 16:02:00 +0330

    ➕ tailwindcss added

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen flex flex-col">
+    <h1 class="text-red-600 text-2xl">tailwind workshop</h1>
   </div>
 </template>
