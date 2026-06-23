bceb354d22902e62f8afb7e2e060cbe42350c227
Mohammad Valadi <movascript@gmail.com>
2026-06-23 18:02:18 +0330

    ✨ icon manager component alogside header used icon svg files added

---
+++ app/components/ui/Icon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// this aint my ideal cause of missing type safetely
+// but i feel its better than manual import for each svg file
+const icons = import.meta.glob("~/assets/icons/*.svg", {
+  eager: true,
+  as: "raw",
+  base: "/assets/icons/",
+});
+
+const props = defineProps<{
+  name: keyof typeof icons;
+  class?: string;
+}>();
+
+const icon = computed(() => icons[`./${props.name}.svg`]);
+</script>
+
+<template>
+  <span v-if="!icon" class="bg-red-500 text-white"
+    >Icon({{ props.name }}) not found</span
+  >
+  <span v-else :class="props.class" v-html="icon" />
+</template>
