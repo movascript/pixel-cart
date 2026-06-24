92c080b9cdb69a7db37a9761584fc67a5dc11298
Mohammad Valadi <movascript@gmail.com>
2026-06-24 14:57:30 +0330

    ♻️ header refactored and dot indicatror for active tab added also now the header shrinks when scrolled down

--- app/components/layout/Header/Nav.vue
+++ app/components/layout/Header/Nav.vue
@@ -1,14 +1,23 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
 <template>
   <nav class="items-center gap-5 absolute left-1/2 -translate-x-1/2">
     <NuxtLink
       v-for="item in headerNavItems"
       :key="item.label"
       :to="item.to"
-      class="flex text-sm text-nowrap font-medium gap-2 items-center hover:text-primary transition-colors"
+      class="relative flex text-sm text-nowrap font-medium gap-2 items-center hover:text-primary transition-colors"
       active-class="text-primary"
     >
       <UiIcon :name="item.icon" />
       {{ item.label }}
+
+      <span
+        v-if="route.path === item.to"
+        class="absolute left-1/2 top-full mt-2 size-1.25 -translate-x-1/2 rounded-full bg-primary"
+      />
     </NuxtLink>
   </nav>
 </template>
